7b8d45a2807f5fbae4512c072f0e1ca3854eb982
rzitomer <richiezitomer@gmail.com>
2018-11-16 17:48:33 -0800

    Clean up load_data script, add documentation, etc.

--- src/load_data.py
+++ src/load_data.py
@@ -1,12 +1,17 @@
-import pandas as pd
+# load_data.py
+# Richie Zitomer and Ayla Pearson, Nov 2018
+#
+# This script outputs the first 100 rows of our input data on wine into
+# the docs/ directory. It takes no arguements.
+#
+# Usage: python load_data.py
 
-# Usage python load_data.property
-# This will output the first 100 rows of our data in the docs/ directory
+import pandas as pd
 
 def main():
     wine_data = pd.read_csv('../data/winemag-data-130k-v2.csv.zip',
                 compression='zip',
-                index_col=0) # Should I hardcode filename here or make it an arg?
+                index_col=0)
     wine_data.head(100).to_csv('../docs/wine_data_first_100_rows.csv',
                                 index=False)
 
