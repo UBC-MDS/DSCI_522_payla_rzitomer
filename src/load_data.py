f5e0caac4043e225a991289770131e76f956b084
rzitomer <richiezitomer@gmail.com>
2018-11-22 12:47:44 -0800

    Add the first draft of the explore data script, along with the results; update load_data script to take in arguements

--- src/load_data.py
+++ src/load_data.py
@@ -1,24 +1,30 @@
 # load_data.py
 # Richie Zitomer and Ayla Pearson, Nov 2018
 #
-# This script outputs the first 100 rows of our input data on wine into
-# the docs/ directory. It takes no arguements.
+# This script loads in the data, gets rid of irrelevant features,
+# makes dummy variables, makes target variable, and outputs the
+# cleaned dataframe to a csv.
 #
-# Usage: python load_data.py
+# Usage: python load_data.py input_file output_file
+# Example usage: python load_data.py 'data/winemag-data-130k-v2.csv.zip' 'data/wine_data_cleaned.csv'
 
 import pandas as pd
+import argparse
 
-input_file = '../data/winemag-data-130k-v2.csv.zip'
-output_file = '../data/wine_data_cleaned.csv'
+parser = argparse.ArgumentParser()
+parser.add_argument('input_file')
+parser.add_argument('output_file')
+args = parser.parse_args()
+
 
 def main():
-    wine_data = pd.read_csv(input_file, index_col=0)
+    wine_data = pd.read_csv(args.input_file, index_col=0)
     wine_data_relev_features = wine_data.drop(
         ['description', 'taster_name', 'taster_twitter_handle', 'title', 'designation', 'region_2', 'winery'], axis=1)
     wine_data_cleaned = wine_data_relev_features.dropna()
     wine_data_cleaned_with_dummies = pd.get_dummies(wine_data_cleaned)
-    wine_data_cleaned_with_dummies['greater_than_90'] = wine_data_cleaned_with_dummies['points'] >= 90
-    wine_data_cleaned_with_dummies.to_csv(output_file, index=False)
+    wine_data_cleaned_with_dummies['greater_than_90'] = wine_data_cleaned_with_dummies['points'] > 90
+    wine_data_cleaned_with_dummies.to_csv(args.output_file, index=False)
 
 
 
