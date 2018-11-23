cb44e999a8446d3b4edb138b6f10f9b26fb67068
rzitomer <richiezitomer@gmail.com>
2018-11-23 15:10:40 -0800

    Updates to explore data: accepts arguements, outputs variety figure

--- src/explore_data.py
+++ src/explore_data.py
@@ -5,16 +5,19 @@
 # makes dummy variables, makes target variable, and outputs the
 # cleaned dataframe to a csv.
 #
-# Usage: python src/explore_data.py input_file output_file
-# Example usage: python load_data.py '../data/winemag-data-130k-v2.csv.zip' '../data/wine_data_cleaned.csv'
+# Usage: python src/explore_data.py input_file output_file_prefix
+# Example usage: python src/explore_data.py 'data/wine_data_cleaned.csv' 'results/viz_'
 import pandas as pd
+import argparse
 import matplotlib.pyplot as plt
 
-file_name = 'data/wine_data_cleaned.csv'
-output_file_prefix = 'results/viz_'
+parser = argparse.ArgumentParser()
+parser.add_argument('input_file')
+parser.add_argument('output_file_prefix')
+args = parser.parse_args()
 
 # Load data
-wine_data_cleaned = pd.read_csv(file_name, index_col=0)
+wine_data_cleaned = pd.read_csv(args.input_file, index_col=0)
 
 ## Look at by country
 france = wine_data_cleaned[wine_data_cleaned.country_France==1]
@@ -23,7 +26,7 @@ italy = wine_data_cleaned[wine_data_cleaned.country_Italy==1]
 spain = wine_data_cleaned[wine_data_cleaned.country_Spain==1]
 portugal = wine_data_cleaned[wine_data_cleaned.country_Portugal==1]
 
-x = ['USA','France','Italy','Spain','Portugal']
+x = ['USA', 'France', 'Italy', 'Spain', 'Portugal']
 y = [100*sum(us.greater_than_90)/len(us),
      100*sum(france.greater_than_90)/len(france),
      100*sum(italy.greater_than_90)/len(italy),
@@ -33,31 +36,32 @@ y = [100*sum(us.greater_than_90)/len(us),
 
 fig, ax = plt.subplots()
 
-plt.bar(x,y)
+plt.bar(x, y)
 plt.axhline(100*sum(wine_data_cleaned.greater_than_90)/len(wine_data_cleaned),
             c='r', label="Overall Average")
-# Also plot an average
 
+# Also plot an average
 ax.set_ylabel('Wines With a Rating > 90 Points (%)')
 ax.set_xlabel('Country')
 ax.set_title('Wine Quality for Top 5 Most Common Country')
 ax.legend()
 
-plt.savefig(output_file_prefix + 'countries.png')
+plt.savefig(args.output_file_prefix + 'countries.png')
+
 
 # Look at by price
 greater = wine_data_cleaned[wine_data_cleaned.greater_than_90]
 less_than = wine_data_cleaned[~wine_data_cleaned.greater_than_90]
 
 fig, ax = plt.subplots()
-plt.boxplot([less_than.price,greater.price],showfliers=False)
+plt.boxplot([less_than.price, greater.price],showfliers=False)
 plt.xticks([1, 2], ['<= 90', '> 90'])
 
 ax.set_ylabel('Price')
 ax.set_xlabel('WineEnthusiast Rating')
 ax.set_title('Wine Quality by Price')
 
-plt.savefig(output_file_prefix + 'price_boxplot.png')
+plt.savefig(args.output_file_prefix + 'price_boxplot.png')
 
 greater = wine_data_cleaned[wine_data_cleaned.greater_than_90]
 less_than = wine_data_cleaned[~wine_data_cleaned.greater_than_90]
@@ -73,4 +77,35 @@ ax.set_xlabel('WineEnthusiast Rating')
 ax.set_title('Wine Quality by Price')
 ax.legend()
 
-plt.savefig(output_file_prefix + 'price_less_than_100_hist.png')
+plt.savefig(args.output_file_prefix + 'price_less_than_100_hist.png')
+
+# Look at by variety
+pinot_noir = wine_data_cleaned[wine_data_cleaned['variety_Pinot Noir']==1]
+chardonnay = wine_data_cleaned[wine_data_cleaned['variety_Chardonnay']==1]
+cabernet_sauvignon = wine_data_cleaned[wine_data_cleaned['variety_Cabernet Sauvignon']==1]
+red_blend = wine_data_cleaned[wine_data_cleaned['variety_Red Blend']==1]
+bordeaux_style_red_blend = wine_data_cleaned[wine_data_cleaned['variety_Bordeaux-style Red Blend']==1]
+
+x = ['Pinot Noir','Chardonnay','Caberney Sauvignon','Red Blend','Bordeaux Style Red Blend']
+y = [100*sum(pinot_noir.greater_than_90)/len(pinot_noir),
+     100*sum(chardonnay.greater_than_90)/len(chardonnay),
+     100*sum(cabernet_sauvignon.greater_than_90)/len(cabernet_sauvignon),
+     100*sum(red_blend.greater_than_90)/len(red_blend),
+     100*sum(bordeaux_style_red_blend.greater_than_90)/len(bordeaux_style_red_blend)
+    ]
+
+fig, ax = plt.subplots()
+
+plt.bar(x,y)
+plt.axhline(100*sum(wine_data_cleaned.greater_than_90)/len(wine_data_cleaned),
+            c='r', label="Overall Average")
+
+# Also plot an average
+ax.set_ylabel('Wines With a Rating > 90 Points (%)')
+ax.set_xlabel('Variety')
+ax.set_title('Wine Quality for Top 5 Most Common Varieties')
+ax.legend()
+
+plt.xticks(rotation=90)
+
+plt.savefig(args.output_file_prefix + 'variety.png', bbox_inches="tight")
