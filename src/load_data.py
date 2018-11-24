3de728d6906880a73600b9827cebfe8a32d8da01
rzitomer <richiezitomer@gmail.com>
2018-11-24 12:00:42 -0800

    Finalizing everything; minor copy edits; add more comments to load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -18,13 +18,20 @@ args = parser.parse_args()
 
 
 def main():
+    # Load in the data
     wine_data = pd.read_csv(args.input_file, index_col=0)
+
+    # Drop features and rows not used in analysis
     wine_data_relev_features = wine_data.drop(
         ['region_1','description', 'taster_name', 'taster_twitter_handle', 'title', 'designation', 'region_2', 'winery'], axis=1)
     wine_data_cleaned = wine_data_relev_features.dropna()
+
+    # Feature Engineering
     wine_data_cleaned_with_dummies = pd.get_dummies(wine_data_cleaned)
     wine_data_cleaned_with_dummies['greater_than_90'] = wine_data_cleaned_with_dummies['points'] >= 90
     wine_data_cleaned_with_dummies = wine_data_cleaned_with_dummies.drop('points', axis=1)
+
+    # Output file
     wine_data_cleaned_with_dummies.to_csv(args.output_file, index=False)
 
 
