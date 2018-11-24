b66d30bbd50c9fbb163562756d8d8dccbc7961f9
Ayla Pearson <aylapearson23@gmail.com>
2018-11-24 09:23:50 -0800

    added comments to script files

--- src/result_plots.py
+++ src/result_plots.py
@@ -1,10 +1,10 @@
 # result_plots.py
 # Richie Zitomer and Ayla Pearson, Nov 2018
 #
-# This script loads in the ranking for each parameter,
+# This script loads in the ranking data for each parameter,
 # creates a bar plot of the top 20 predictors. It also creates a bar plot
 # zoomed in with the top 19 predictors. 
-# 
+
 
 # Usage: python src/result_plots.py input_file output_file_directory
 # Example: python src/result_plots.py results/rank.csv results/
@@ -21,11 +21,17 @@ args = parser.parse_args()
 
 def main():
     
+    # reads in feature ranking csv
     data = pd.read_csv(args.input_file)
+    
+    # sorts the csv so higher ranked feature are first
     sorted_by_rank = data.sort_values(by = 'rank_value', ascending = False)
+    
+    # creates lists of the top 20 features and the top 20 without the 1st feature
     top_20 = sorted_by_rank[:20]
     top_19 = top_20[1:20]
     
+    # creates bar plot of the top 20 and top 19 features, it creates them side-by-side as subplots
     fig, ax = plt.subplots(ncols=2, figsize=(14,6))
     top_20_plot = top_20.plot.bar(y='rank_value', 
                                  x='predictor',  
@@ -45,6 +51,8 @@ def main():
     top_19_plot.set_ylabel("Rank Value")
     top_19_plot.set_xlabel("Predictors")
     plt.tight_layout()
+    
+    # writes the plots to file, this is the output of the script
     plt.savefig(args.output_file + "rank_plots.png")
     
 
